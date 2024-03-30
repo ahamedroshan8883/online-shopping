0666e32c48740157a67892848f582af0f7736b00
ahamedroshan8883 <119722174+ahamedroshan8883@users.noreply.github.com>
2024-03-30 16:10:02 +0530

    created home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react"
+import '../Home/Home.css'
+import fashionimg from '../../assets/homepage-banner-with-bg-removebg-preview.png';
+
 
 export default function Home(){
   return (<>
-    <h1>Home</h1>
+    <div id="homepage">
+        <div className="content-text">
+        <span className="samll-txt">NEW ARRAIVALS ONLY</span><br />
+        <span className="large-txt">new 👋 Collections for Every one</span>
+        </div>
+        <img src={fashionimg} alt="" className="homepageimg"/>
+      </div>
   </>)
 };
